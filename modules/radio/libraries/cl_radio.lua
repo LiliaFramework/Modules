08f5afbb2973a51c103ef74ed3f583740d88bd8e
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-12-17 16:35:48 +0000

    Fixed Bug with Radio & Removed some obsolete systems

--- modules/radio/libraries/cl_radio.lua
+++ modules/radio/libraries/cl_radio.lua
@@ -3,7 +3,7 @@ surface.CreateFont(
     "liaRadioFont",
     {
         font = "Lucida Sans Typewriter",
-        size = math.max(ScreenScale(7), 17),
+        size =  17),
         weight = 100
     }
 )
