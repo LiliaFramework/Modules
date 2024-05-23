f4cb6bca8ade50f8faa79851fb2cf6a33251fa10
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-05-24 00:45:18 +0100

    Added DownTime

---
+++ modules/downtime/config/server.lua
@@ -0,0 +1,2 @@
+MODULE.RPMinimumPlayerCount = 5
+MODULE.EnableDownTime = false
---
+++ modules/downtime/libraries/server.lua
@@ -0,0 +1,18 @@
+local isDownTime = false
+
+function MODULE:PlayerLoadedChar()
+    if not self.EnableDownTime then return end
+
+    local playerCount = player.GetCount()
+
+    if playerCount < self.RPMinimumPlayerCount then
+        if not isDownTime then
+            isDownTime = true
+        end
+    else
+        if isDownTime then
+            lia.util.notify("Downtime is over.", player.GetAll())
+            isDownTime = false
+        end
+    end
+end
