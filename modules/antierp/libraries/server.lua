a0ea6df3507b029b20139a4e2ceb8915bf482759
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-06-19 18:36:38 +0100

    Update server.lua

--- modules/antierp/libraries/server.lua
+++ modules/antierp/libraries/server.lua
@@ -2,7 +2,7 @@
     if self.DetectedChatTypes[chatType] then
         for _, v in pairs(self.NotAllowedWords) do
             if string.find(message, v) then
-                message = string.format("ERPER DETECTED: (%s) (%s) ", client:GetCharacter():GetName(), message)
+                message = string.format("ERPER DETECTED: (%s) (%s) ", client:getChar():getName(), message)
                 for _, ply in pairs(player.GetAll()) do
                     if ply:IsAdmin() then ply:PrintMessage(HUD_PRINTTALK, message) end
                 end
