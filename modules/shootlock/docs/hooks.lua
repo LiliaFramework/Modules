0d68a8af92ea21eadb32ff4ec234e676055754ff
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-07-21 15:15:45 +0100

    Linting

--- modules/shootlock/docs/hooks.lua
+++ modules/shootlock/docs/hooks.lua
@@ -1,8 +1,9 @@
 --- Hook Documentation for ShootLock Module.
 -- @hooks ShootLock
 
---- Called to determine if a player is allowed to bust a lock in a certain entity.
+--- Called to determine if a player is allowed to bust a lock on a specific entity.
 -- @realm server
--- @client client Player The player who is attempting to bust the lock entity.
+-- @client client Player The player who is attempting to bust the lock.
+-- @entity entity Entity The entity that the player is attempting to bust the lock on.
 function CanPlayerBustLock(client, entity)
 end
