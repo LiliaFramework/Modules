4c8da9fcc66db6e987b369f32c2712da2dcc464a
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-07-22 00:23:24 +0100

    Added Some Manual Persistances

--- modules/crafting/entities/entities/craftingbase/shared.lua
+++ modules/crafting/entities/entities/craftingbase/shared.lua
@@ -1,13 +1,14 @@
-﻿CraftingTables = CraftingTables or {}
+﻿local MODULE = MODULE
+CraftingTables = CraftingTables or {}
 ENT.Type = "anim"
 ENT.Author = "Samael"
 ENT.Contact = "@liliaplayer"
 ENT.Instructions = "Press USE whilst looking at table to open crafting menu."
 ENT.Spawnable = false
 ENT.AdminOnly = true
-ENT.DisableDuplicator = true
 ENT.Category = "Crafting"
 ENT.IsCraftingTable = true
+ENT.IsPersistent = MODULE.PermanentCraftingTables
 function ENT:getInv()
     return lia.inventory.instances[self:getNetVar("id", 0)]
 end
--- modules/crafting/module.lua
+++ modules/crafting/module.lua
@@ -2,3 +2,4 @@
 MODULE.author = "76561198312513285"
 MODULE.discord = "@liliaplayer"
 MODULE.desc = "Advanced crafting system."
+MODULE.PermanentCraftingTables = true
