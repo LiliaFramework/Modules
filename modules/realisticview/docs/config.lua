6290a5713e6a2718c9350a3c105856b27e75de54
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-07-29 00:59:00 +0100

    Fixed Old Documentations

--- modules/realisticview/docs/config.lua
+++ modules/realisticview/docs/config.lua
@@ -4,4 +4,4 @@
 --- This table defines the default settings for the Realistic View Module.
 -- @realm shared
 -- @table Configuration
--- @field FirstPersonViewEnabled bool Indicates whether the first-person view is enabled
+-- @field FirstPersonViewEnabled bool Indicates whether the first-person view is enabled | **bool**
