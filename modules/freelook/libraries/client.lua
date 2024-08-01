992707f012df4b51bfacbbc296c8e075f9701c32
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-08-01 05:06:10 +0100

    Linting

--- modules/freelook/libraries/client.lua
+++ modules/freelook/libraries/client.lua
@@ -1,24 +1,24 @@
 local FreeLookEnabled = CreateClientConVar("freelook_enabled", 0, true)
 local freelooking, LookX, LookY, InitialAng, CoolAng, ZeroAngle = false, 0, 0, Angle(), Angle(), Angle()
 
-function MODULE:Isinsights(ply)
-    local weapon = ply:GetActiveWeapon()
-    return self.FreelookBlockADS and (ply:KeyDown(IN_ATTACK2) or (weapon.GetInSights and weapon:GetInSights()) or (weapon.ArcCW and weapon:GetState() == ArcCW.STATE_SIGHTS) or (weapon.GetIronSights and weapon:GetIronSights()))
+function MODULE:Isinsights(client)
+    local weapon = client:GetActiveWeapon()
+    return self.FreelookBlockADS and (client:KeyDown(IN_ATTACK2) or (weapon.GetInSights and weapon:GetInSights()) or (weapon.ArcCW and weapon:GetState() == ArcCW.STATE_SIGHTS) or (weapon.GetIronSights and weapon:GetIronSights()))
 end
 
-function MODULE:Holdingbind(ply)
+function MODULE:Holdingbind(client)
     if not input.LookupBinding("freelook") then
-        return ply:KeyDown(IN_WALK)
+        return client:KeyDown(IN_WALK)
     else
         return freelooking
     end
 end
 
-function MODULE:CalcView(ply, origin, angles, fov)
-    if not ply:getChar() or not FreeLookEnabled then return end
+function MODULE:CalcView(client, _, angles, IN_WALK)
+    if not client:getChar() or not FreeLookEnabled then return end
     local smoothness = math.Clamp(self.FreelookSmooth, 0.1, 2)
     CoolAng = LerpAngle(0.15 * smoothness, CoolAng, Angle(LookY, -LookX, 0))
-    if not self:Holdingbind(ply) and CoolAng.p < 0.05 and CoolAng.p > -0.05 or self:Isinsights(ply) and CoolAng.p < 0.05 and CoolAng.p > -0.05 or not system.HasFocus() or ply:ShouldDrawLocalPlayer() then
+    if not self:Holdingbind(client) and CoolAng.p < 0.05 and CoolAng.p > -0.05 or self:Isinsights(client) and CoolAng.p < 0.05 and CoolAng.p > -0.05 or not system.HasFocus() or client:ShouldDrawLocalPlayer() then
         InitialAng = angles + CoolAng
         LookX, LookY = 0, 0
         CoolAng = ZeroAngle
@@ -29,7 +29,7 @@ function MODULE:CalcView(ply, origin, angles, fov)
     angles.y = angles.y + CoolAng.y
 end
 
-function MODULE:CalcViewModelView(wep, vm, oPos, oAng, pos, ang)
+function MODULE:CalcViewModelView(wep, _, _, _, _, ang)
     local lp = LocalPlayer()
     if not lp:getChar() or not FreeLookEnabled then return end
     local MWBased = wep.m_AimModeDeltaVelocity and -1.5 or 1
@@ -37,7 +37,7 @@ function MODULE:CalcViewModelView(wep, vm, oPos, oAng, pos, ang)
     ang.y = ang.y + CoolAng.y / 2.5 * MWBased
 end
 
-function MODULE:InputMouseApply(cmd, x, y, ang)
+function MODULE:InputMouseApply(cmd, x, y)
     local lp = LocalPlayer()
     if not lp:getChar() or not FreeLookEnabled then return end
     if not self:Holdingbind(lp) or self:Isinsights(lp) or lp:ShouldDrawLocalPlayer() then
@@ -52,11 +52,11 @@ function MODULE:InputMouseApply(cmd, x, y, ang)
     return true
 end
 
-function MODULE:StartCommand(ply, cmd)
-    if not ply:IsPlayer() or not ply:Alive() then return end
-    if not ply:getChar() then return end
+function MODULE:StartCommand(client, cmd)
+    if not client:IsPlayer() or not client:Alive() then return end
+    if not client:getChar() then return end
     if not self.FreelookBlockFire then return end
-    if not self:Holdingbind(ply) or self:Isinsights(ply) or ply:ShouldDrawLocalPlayer() then return end
+    if not self:Holdingbind(client) or self:Isinsights(client) or client:ShouldDrawLocalPlayer() then return end
     cmd:RemoveKey(IN_ATTACK)
 end
 
@@ -70,5 +70,5 @@ function MODULE:SetupQuickMenu(menu)
     end, FreeLookEnabled:GetBool())
 end
 
-concommand.Add("+freelook", function(ply, cmd, args) freelooking = true end)
-concommand.Add("-freelook", function(ply, cmd, args) freelooking = false end)
+concommand.Add("+freelook", function() freelooking = true end)
+concommand.Add("-freelook", function() freelooking = false end)
