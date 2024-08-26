a3d50d2e6c7bf67aa82cd78179a5eb19cf884412
Samael <33399712+bleonheart@users.noreply.github.com>
2024-08-26 16:44:20 +0100

    Started Tying Localization

---
+++ modules/tying/languages/english.lua
@@ -0,0 +1,10 @@
+﻿NAME = "english"
+LANGUAGE = {
+    isTied = "This person has been tied up.",
+    tying = "Tying",
+    untying = "Untying",
+    beingUntied = "You are being untied.",
+    beingTied = "You are being tied.",
+    restricted = "You have been restrained.",
+    charSearching = "You are already searching for another character, please wait.",
+}
