57387e5a1c3f979ebab4d0d7b1e18155a10cb471
Samael <33399712+bleonheart@users.noreply.github.com>
2024-09-08 19:13:08 +0100

    Updated Methods

--- modules/downtime/libraries/server.lua
+++ modules/downtime/libraries/server.lua
@@ -6,7 +6,7 @@ function MODULE:PlayerLoadedChar()
         if not isDownTime then isDownTime = true end
     else
         if isDownTime then
-            lia.util.notify(L("DowntimeOver"), player.GetAll())
+            lia.notices.notify(L("DowntimeOver"), player.GetAll())
             isDownTime = false
         end
     end
