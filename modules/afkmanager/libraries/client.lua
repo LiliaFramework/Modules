b147f4a51163a0e6e92e21dafcb5b5023cc906b1
Samael <33399712+bleonheart@users.noreply.github.com>
2025-01-27 16:58:31 +0000

    Update client.lua

--- modules/afkmanager/libraries/client.lua
+++ modules/afkmanager/libraries/client.lua
@@ -1,4 +1,5 @@
-﻿local function DrawWarning()
+﻿local MODULE = MODULE
+local function DrawWarning()
     if MODULE.Alpha < 230 then MODULE.Alpha = MODULE.Alpha + (FrameTime() * 200) end
     draw.RoundedBox(0, 0, (ScrH() / 2) - ScreenScale(60), ScrW(), ScreenScale(120), Color(0, 0, 0, MODULE.Alpha))
     draw.DrawText(lia.config.get("AFKWarningHead", "WARNING!"), "AFKKicker120", ScrW() * 0.5, (ScrH() * 0.5) - ScreenScale(50), Color(255, 0, 0, MODULE.Alpha), TEXT_ALIGN_CENTER)
