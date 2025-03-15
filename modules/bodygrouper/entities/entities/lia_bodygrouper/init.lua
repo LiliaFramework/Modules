9b4e9da3163bf1e204eed02fda81f5ce2c4f1778
Samael <33399712+bleonheart@users.noreply.github.com>
2025-03-15 16:02:51 +0000

    Freezed Bodygrouper

--- modules/bodygrouper/entities/entities/lia_bodygrouper/init.lua
+++ modules/bodygrouper/entities/entities/lia_bodygrouper/init.lua
@@ -3,6 +3,8 @@
     self:PhysicsInit(SOLID_VPHYSICS)
     self:SetMoveType(MOVETYPE_VPHYSICS)
     self:SetUseType(SIMPLE_USE)
+    local phys = self:GetPhysicsObject()
+    if IsValid(phys) then phys:EnableMotion(false) end
 end
 
 function ENT:Use(activator)
