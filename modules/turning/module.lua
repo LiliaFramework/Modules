cc687f1a08d88f06c0c09806b199ea3f0016c9c1
eon (bloodycop) <eon.bloodycop@gmail.com>
2025-03-18 00:57:59 +0200

    Rappelling & Turning Module

---
+++ modules/turning/module.lua
@@ -0,0 +1,18 @@
+local MODULE = MODULE
+
+MODULE.name = "Turning"
+MODULE.author = "TankNut & bloodycop"
+MODULE.version = "Stock"
+MODULE.desc = "Adds support for playermodels playing turning animations."
+
+MODULE.supportedModelClasses = {
+	metrocop = true,
+	overwatch = true,
+	citizen_male = true,
+	citizen_female = true
+}
+
+MODULE.activityWhitelist = {
+	[ACT_MP_STAND_IDLE] = true,
+	[ACT_MP_CROUCH_IDLE] = true
+}
