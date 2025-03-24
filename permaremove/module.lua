237b9b74f0eed5df8f85d7d6a7ce0ac34e7203b0
Samael <33399712+bleonheart@users.noreply.github.com>
2025-03-24 13:17:15 +0000

    Versioning

--- permaremove/module.lua
+++ permaremove/module.lua
@@ -1,5 +1,6 @@
 ﻿MODULE.name = "Perma Remove"
 MODULE.author = "Boz [Base Code] & Samael [Minor Optimizations]"
 MODULE.discord = "bozdev"
+MODULE.version = "1.0"
 MODULE.desc = "Allows staff to permanently remove map entities"
 MODULE.Public = true
