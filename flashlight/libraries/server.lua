3e95381dc8101993443523fdbab64b0c932f2de3
Samael <33399712+bleonheart@users.noreply.github.com>
2025-04-21 15:29:04 +0100

    Flashlight Fixes

--- flashlight/libraries/server.lua
+++ flashlight/libraries/server.lua
@@ -3,26 +3,24 @@
     local flashlightEnabled = lia.config.get("FlashlightEnabled", true)
     local flashlightNeedsItem = lia.config.get("FlashlightNeedsItem", true)
     local flashlightCooldown = lia.config.get("FlashlightCooldown", 0.5)
+    if not flashlightEnabled then return false end
+    if (client.FlashlightCooldown or 0) >= CurTime() then return false end
     local hasFlashlight = false
-    if flashlightEnabled and (client.FlashlightCooldown or 0) < CurTime() then
-        if flashlightNeedsItem then
-            for _, item in pairs(client:getChar():getInv():getItems()) do
-                if not item.isFlashlight then continue end
-                if client:getChar():getInv():hasItem(item) then
-                    hasFlashlight = true
-                    break
-                end
+    if flashlightNeedsItem then
+        for _, item in pairs(client:getChar():getInv():getItems()) do
+            if item.isFlashlight then
+                hasFlashlight = true
+                break
             end
-        else
-            hasFlashlight = true
         end
 
-        if hasFlashlight then
-            client:EmitSound(isEnabled and "buttons/button24.wav" or "buttons/button10.wav", 60, isEnabled and 100 or 70)
-            client.FlashlightCooldown = CurTime() + flashlightCooldown
-            client:ConCommand("r_shadows " .. (isEnabled and "1" or "0"))
-            return true
-        end
+        if not hasFlashlight then return false end
+    else
+        hasFlashlight = true
     end
-    return false
+
+    client:EmitSound(isEnabled and "buttons/button24.wav" or "buttons/button10.wav", 60, isEnabled and 100 or 70)
+    client.FlashlightCooldown = CurTime() + flashlightCooldown
+    client:ConCommand("r_shadows " .. (isEnabled and "1" or "0"))
+    return true
 end
