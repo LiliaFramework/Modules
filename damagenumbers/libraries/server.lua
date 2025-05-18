c279c4762643b27a9b210702a425f6ea2813ff0d
Samael <33399712+bleonheart@users.noreply.github.com>
2025-05-18 12:41:56 +0100

    Update server.lua

--- damagenumbers/libraries/server.lua
+++ damagenumbers/libraries/server.lua
@@ -1,4 +1,4 @@
-function MODULE:PostEntityTakeDamage(ent, _, tookDamage)
+function MODULE:PostEntityTakeDamage(ent, dmgInfo, tookDamage)
     if not tookDamage or not ent:IsPlayer() then return end
     local atk = dmgInfo:GetAttacker()
     if not IsValid(atk) or not atk:IsPlayer() then return end
