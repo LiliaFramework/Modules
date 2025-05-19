db9438950e007b3a221082a28a9e9db1c94f39fd
Samael <33399712+bleonheart@users.noreply.github.com>
2025-05-19 17:28:45 +0100

    Delete english.lua

--- loyalism/languages/english.lua
+++
@@ -1,6 +0,0 @@
-﻿NAME = "english"
-LANGUAGE = {
-    partyTierUpdated = "You have updated %s's Party Tier to %s.",
-    partyTierRemoved = "%s has removed your Party Tier!",
-    partyTierSet = "You have been set as Party Tier %s.",
-}
