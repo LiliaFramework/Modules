2349a77cc2dc6431e4ca9bdccc0b3e50d822051d
Samael <33399712+bleonheart@users.noreply.github.com>
2025-05-20 16:57:15 +0100

    Updates

--- developmentserver/module.lua
+++ developmentserver/module.lua
@@ -1,6 +1,6 @@
 ﻿MODULE.name = "Development Server"
 MODULE.author = "Samael"
 MODULE.discord = "@liliaplayer"
-MODULE.version = "1.0.2"
+MODULE.version = "1.0"
 MODULE.desc = "Adds the option for a Development Server, which can run specific functions."
 MODULE.Public = true
