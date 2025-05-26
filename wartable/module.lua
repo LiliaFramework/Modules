d9543cd0b05e8f9ce716feba3400bae8e1208d44
Samael <33399712+bleonheart@users.noreply.github.com>
2025-05-27 00:07:06 +0100

    Update module.lua

--- wartable/module.lua
+++ wartable/module.lua
@@ -3,5 +3,5 @@ MODULE.author = "Samael"
 MODULE.discord = "@liliaplayer"
 MODULE.version = "1.0.3"
 MODULE.desc = "Adds a interactive War Table"
-MODULE.WorkshopContent = {"3431351432"}
+MODULE.WorkshopContent = "3431351432"
 MODULE.Public = true
