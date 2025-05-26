7cc3c92f19758b51917dd3dfda0023abbebc6b0f
Samael <33399712+bleonheart@users.noreply.github.com>
2025-05-26 14:18:12 +0100

    Versioning

--- donator/module.lua
+++ donator/module.lua
@@ -1,6 +1,6 @@
 ﻿MODULE.name = "Donator"
 MODULE.author = "Samael"
 MODULE.discord = "@liliaplayer"
-MODULE.version = "1.0.3"
+MODULE.version = "1.0.4"
 MODULE.desc = "Adds Several Donation Related Libraries."
 MODULE.Public = true
