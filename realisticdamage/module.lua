23b8163d78e7126c5841329c808413956e7a40a1
Samael <33399712+bleonheart@users.noreply.github.com>
2025-06-01 03:03:52 +0100

    Realistic Damage

---
+++ realisticdamage/module.lua
@@ -0,0 +1,6 @@
+﻿MODULE.name = "Realistic Damage"
+MODULE.author = "Samael"
+MODULE.discord = "@liliaplayer"
+MODULE.version = "1.0"
+MODULE.Public = true
+MODULE.desc = "Adds Damage Scalers for Realism"
