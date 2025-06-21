945c55dce5de79d34f1c28230185ac91af880638
Samael <33399712+bleonheart@users.noreply.github.com>
2025-06-21 14:12:58 +0100

    Update client.lua

--- tying/submodules/tyingsearch/libraries/client.lua
+++ tying/submodules/tyingsearch/libraries/client.lua
@@ -1,7 +1,15 @@
-﻿function MODULE:CanPlayerViewInventory()
-    if LocalPlayer():IsBeingSearched() then return false end
+﻿local MODULE = MODULE
+function MODULE:CanPlayerViewInventory(client)
+    if not IsValid(client) then return false end
+    local character = client:getChar()
+    if not character then return false end
+    if client:IsBeingSearched() then return false end
+    return true
 end
 
 function MODULE:DrawCharInfo(client, _, info)
+    if not IsValid(client) then return end
+    local character = client:getChar()
+    if not character then return end
     if client:IsHandcuffed() and client:IsBeingSearched() then info[#info + 1] = {L("beingSearched"), Color(245, 215, 110)} end
 end
