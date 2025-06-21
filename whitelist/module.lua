51dfe8298280d9db357ea3f25b18ef621b8899fa
Samael <33399712+bleonheart@users.noreply.github.com>
2025-06-21 13:40:07 +0100

    Removed unused space

--- whitelist/module.lua
+++ whitelist/module.lua
@@ -3,5 +3,4 @@ MODULE.author = "Samael"
 MODULE.discord = "@liliaplayer"
 MODULE.version = "1.0.1"
 MODULE.desc = "Adds a Server Whitelist"
-MODULE.identifier = "WhitelistCore"
 MODULE.Public = true
