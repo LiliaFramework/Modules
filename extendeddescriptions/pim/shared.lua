149fb6483ca095a84ef1f097c184b697976e8a94
Samael <33399712+bleonheart@users.noreply.github.com>
2025-06-23 22:52:44 +0100

    Extended Desc

--- extendeddescriptions/pim/shared.lua
+++ extendeddescriptions/pim/shared.lua
@@ -1,8 +1,7 @@
 ﻿AddInteraction(L("openDetDescLabel"), {
-    runServer = true,
+    runServer = false,
     shouldShow = function(_, target) return IsValid(target) end,
     onRun = function(client, target)
-        if not SERVER then return end
         net.Start("OpenDetailedDescriptions")
         net.WriteEntity(target)
         net.WriteString(target:getChar():getData("textDetDescData", nil) or L("openDetDescFallback"))
