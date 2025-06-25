7458ba0538a9b0b7b15fffac8c970600c0c36828
Samael <33399712+bleonheart@users.noreply.github.com>
2025-06-25 07:43:25 +0100

    Localize autorestarter restart message

--- autorestarter/libraries/client.lua
+++ autorestarter/libraries/client.lua
@@ -6,6 +6,6 @@ function MODULE:HUDPaint()
     if remaining > 0 and remaining <= interval * 0.25 then
         local m = math.floor(remaining / 60)
         local s = remaining % 60
-        draw.SimpleTextOutlined(string.format("Server Restarting in: %02d:%02d", m, s), lia.config.get("RestartCountdownFont"), ScrW() - 10, 10, Color(255, 255, 255), TEXT_ALIGN_RIGHT, TEXT_ALIGN_TOP, 1, Color(0, 0, 0))
+        draw.SimpleTextOutlined(L("restartCountdown", m, s), lia.config.get("RestartCountdownFont"), ScrW() - 10, 10, Color(255, 255, 255), TEXT_ALIGN_RIGHT, TEXT_ALIGN_TOP, 1, Color(0, 0, 0))
     end
 end
