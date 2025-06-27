eef6bf32f995e1873deaeeeeab487a04d32474b2
Samael <33399712+bleonheart@users.noreply.github.com>
2025-06-27 03:07:49 +0100

    Update shared.lua

--- weightinv/submodules/storage/libraries/shared.lua
+++ weightinv/submodules/storage/libraries/shared.lua
@@ -3,16 +3,18 @@
 end
 
 function MODULE:InitializeStorage(entity)
-    if self.Vehicles[entity] then return end
-    local key = entity:IsVehicle() and "vehicle" or entity:GetModel():lower()
+    if not IsValid(entity) or self.Vehicles[entity] then return end
+    local model = entity:GetModel()
+    if not model then return end
+    local key = entity:IsVehicle() and "vehicle" or model:lower()
     local def = self.StorageDefinitions[key]
     if not def then return end
     self.Vehicles[entity] = true
     if SERVER then
         entity.receivers = {}
-        lia.inventory.instance(def.invType, {
-            maxWeight = def.weight
-        }):next(function(inv)
+        local params = def.invData or {}
+        if def.weight then params.maxWeight = def.weight end
+        lia.inventory.instance(def.invType, params):next(function(inv)
             inv.isStorage = true
             entity:setNetVar("inv", inv:getID())
             hook.Run("StorageInventorySet", self, inv, entity:IsVehicle())
