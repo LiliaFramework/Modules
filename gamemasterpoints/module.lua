51fec2b47a29fd4547744ef30c92e97f1eb8ccb2
Samael <33399712+bleonheart@users.noreply.github.com>
2025-07-09 02:09:29 +0100

    Fix MODULE variable ordering

--- gamemasterpoints/module.lua
+++ gamemasterpoints/module.lua
@@ -1,6 +1,7 @@
 MODULE.name = "Gamemaster Teleport Points"
 MODULE.author = "Samael"
 MODULE.discord = "@liliaplayer"
-MODULE.version = "1.0.1"MODULE.desc = "Allow GMs to teleport to predefined points on the map"
+MODULE.version = "1.0.1"
+MODULE.desc = "Allow GMs to teleport to predefined points on the map"
 
 MODULE.Public = true
