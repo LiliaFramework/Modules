cb91a2e6da54e798d1f67e2f2df8c45989fdfbb5
Samael <33399712+bleonheart@users.noreply.github.com>
2025-07-10 07:37:44 +0100

    Update cl_init.lua

--- cigs/entities/weapons/weapon_ciga/cl_init.lua
+++ cigs/entities/weapons/weapon_ciga/cl_init.lua
@@ -53,17 +53,11 @@ function SWEP:GetViewModelPosition(pos, ang)
 	if not LocalPlayer().cigaArmTime then LocalPlayer().cigaArmTime = 0 end
 	local lerp = math.Clamp((os.clock() - LocalPlayer().cigaArmTime) * 3, 0, 1)
 	if LocalPlayer().cigaArm then lerp = 1 - lerp end
-	local newpos = LerpVector(lerp, vmpos1, vmpos2)
-	local newang = LerpVector(lerp, vmang1, vmang2)
-	local difvec = Vector(-10, -3.5, -12)
-	local orig = Vector(0, 0, 0)
-	local topos = orig + difvec
-	local difang = Vector(-30, 0, 0)
-	local origang = Vector(0, 0, 0)
-	local toang = origang + difang
-	local newpos = LerpVector(lerp, topos, orig)
-	local newang = LerpVector(lerp, toang, origang)
-	newang = Angle(newang.x, newang.y, newang.z)
+	local topos = Vector(-10, -3.5, -12)
+	local toang = Vector(-30, 0, 0)
+	local newpos = LerpVector(lerp, topos, Vector(0, 0, 0))
+	local vecang = LerpVector(lerp, toang, Vector(0, 0, 0))
+	local newang = Angle(vecang.x, vecang.y, vecang.z)
 	pos, ang = LocalToWorld(newpos, newang, pos, ang)
 	return pos, ang
 end
@@ -107,18 +101,14 @@ net.Receive("cigaArm", function()
 	if not IsValid(ply) then return end
 	if ply.cigaArm ~= z then
 		if z then
-			timer.Simple(0.3, function()
-				if not IsValid(ply) then return end
-				if ply.cigaArm then ply:EmitSound("ciga_inhale") end
-			end)
+			timer.Simple(0.3, function() if IsValid(ply) and ply.cigaArm then ply:EmitSound("ciga_inhale") end end)
 		else
 			ply:StopSound("ciga_inhale")
 		end
 
 		ply.cigaArm = z
 		ply.cigaArmTime = os.clock()
-		local m = 0
-		if z then m = 1 end
+		local m = z and 1 or 0
 		for i = 0, 9 do
 			timer.Simple(i / 30, function() MODULE.ciga_interpolate_arm(ply, math.abs(m - (9 - i) / 10), z and 0 or 0.2) end)
 		end
@@ -143,33 +133,28 @@ function MODULE.ciga_interpolate_arm(ply, mult, mouth_delay)
 	if not b1 or not b2 then return end
 	ply:ManipulateBoneAngles(b1, Angle(20 * mult, -62 * mult, 10 * mult))
 	ply:ManipulateBoneAngles(b2, Angle(-5 * mult, -10 * mult, 0))
-	if mult == 1 then
-		ply.cigaArmFullyUp = true
-	else
-		ply.cigaArmFullyUp = false
-	end
+	ply.cigaArmFullyUp = mult == 1
 end
 
 hook.Add("InitPostEntity", "cigaMouthMoveSetup", function()
 	timer.Simple(1, function()
-		if ciga_OriginalMouthMove ~= nil then return end
+		if ciga_OriginalMouthMove then return end
 		ciga_OriginalMouthMove = GAMEMODE.MouthMoveAnimation
 		function GAMEMODE:MouthMoveAnimation(ply)
-			if (ply.cigaMouthOpenAmt or 0) == 0 and (ply.cigaTalkingEndtime or 0) < CurTime() then return ciga_OriginalMouthMove(GAMEMODE, ply) end
-			local FlexNum = ply:GetFlexNum() - 1
-			if FlexNum <= 0 then return end
-			for i = 0, FlexNum - 1 do
-				local Name = ply:GetFlexName(i)
-				if Name == "jaw_drop" or Name == "right_part" or Name == "left_part" or Name == "right_mouth_drop" or Name == "left_mouth_drop" then ply:SetFlexWeight(i, math.max((ply.cigaMouthOpenAmt or 0) * 0.5, math.Clamp(((ply.cigaTalkingEndtime or 0) - CurTime()) * 3.0, 0, 1) * math.Rand(0.1, 0.8))) end
+			if (ply.cigaMouthOpenAmt or 0) == 0 and (ply.cigaTalkingEndtime or 0) < CurTime() then return ciga_OriginalMouthMove(self, ply) end
+			local flexCount = ply:GetFlexNum() - 1
+			if flexCount <= 0 then return end
+			for i = 0, flexCount - 1 do
+				local name = ply:GetFlexName(i)
+				if name == "jaw_drop" or name == "right_part" or name == "left_part" or name == "right_mouth_drop" or name == "left_mouth_drop" then ply:SetFlexWeight(i, math.max((ply.cigaMouthOpenAmt or 0) * 0.5, math.Clamp(((ply.cigaTalkingEndtime or 0) - CurTime()) * 3, 0, 1) * math.Rand(0.1, 0.8))) end
 			end
 		end
 	end)
 end)
 
-function MODULE.ciga_do_pulse(ply, amt, spreadadd, fx)
+function MODULE.ciga_do_pulse(ply, amt, _, fx)
 	if not IsValid(ply) then return end
 	if ply:WaterLevel() == 3 then return end
-	if not spreadadd then spreadadd = 0 end
 	local attachid = ply:LookupAttachment("eyes")
 	cigaParticleEmitter:SetPos(LocalPlayer():GetPos())
 	local angpos = ply:GetAttachment(attachid) or {
@@ -177,8 +162,7 @@ function MODULE.ciga_do_pulse(ply, amt, spreadadd, fx)
 		Pos = Vector(0, 0, 0)
 	}
 
-	local fwd
-	local pos
+	local fwd, pos
 	if ply ~= LocalPlayer() then
 		fwd = (angpos.Ang:Forward() - angpos.Ang:Up()):GetNormalized()
 		pos = angpos.Pos + fwd * 3.5
@@ -193,7 +177,8 @@ function MODULE.ciga_do_pulse(ply, amt, spreadadd, fx)
 		local particle = cigaParticleEmitter:Add(string.format("particle/smokesprites_00%02d", math.random(7, 16)), pos)
 		if particle then
 			local dir = VectorRand():GetNormalized() * (amt + 5) / 10
-			MODULE.ciga_do_particle(particle, ply:GetVelocity() * 0.25 + (fwd * 9 + dir):GetNormalized() * math.Rand(50, 80) * (amt + 1) * 0.2, fx)
+			local vel = ply:GetVelocity() * 0.25 + (fwd * 9 + dir):GetNormalized() * math.Rand(50, 80) * (amt + 1) * 0.2
+			MODULE.ciga_do_particle(particle, vel, fx)
 		end
 	end
 end
@@ -202,14 +187,11 @@ function MODULE.ciga_do_particle(particle, vel, fx)
 	particle:SetColor(255, 255, 255, 255)
 	if fx == 3 then particle:SetColor(100, 100, 100, 100) end
 	if fx >= 4 then
-		local c = JuicycigaJuices[fx - 3].color
-		if c == nil then c = HSVToColor(math.random(0, 359), 1, 1) end
+		local c = JuicycigaJuices[fx - 3].color or HSVToColor(math.random(0, 359), 1, 1)
 		particle:SetColor(c.r, c.g, c.b, 255)
 	end
 
-	local mega = 1
-	if fx == 2 then mega = 4 end
-	mega = mega * 0.3
+	local mega = (fx == 2 and 4 or 1) * 0.3
 	particle:SetVelocity(vel * mega)
 	particle:SetGravity(Vector(0, 0, 1.5))
 	particle:SetLifeTime(0)
@@ -227,7 +209,7 @@ end
 
 matproxy.Add({
 	name = "cigaTankColor",
-	init = function(self, mat, values) self.ResultTo = values.resultvar end,
+	init = function(self, _, values) self.ResultTo = values.resultvar end,
 	bind = function(self, mat, ent)
 		if not IsValid(ent) then return end
 		if ent:GetClass() == "viewmodel" then
@@ -249,7 +231,7 @@ matproxy.Add({
 
 matproxy.Add({
 	name = "cigaAccentColor",
-	init = function(self, mat, values) self.ResultTo = values.resultvar end,
+	init = function(self, _, values) self.ResultTo = values.resultvar end,
 	bind = function(self, mat, ent)
 		if not IsValid(ent) then return end
 		local o = ent:GetOwner()
@@ -274,21 +256,21 @@ if CLIENT then
 	SWEP.vRenderOrder = nil
 	function SWEP:ViewModelDrawn()
 		local vm = self:GetOwner():GetViewModel()
-		if not IsValid(vm) then return end
-		if not self.VElements then return end
+		if not IsValid(vm) or not self.VElements then return end
 		self:UpdateBonePositions(vm)
 		if not self.vRenderOrder then
 			self.vRenderOrder = {}
-			for k, v in pairs(self.VElements) do
+			for _, key in pairs(self.VElements) do
+				local v = self.VElements[key]
 				if v.type == "Model" then
-					table.insert(self.vRenderOrder, 1, k)
+					table.insert(self.vRenderOrder, 1, key)
 				elseif v.type == "Sprite" or v.type == "Quad" then
-					table.insert(self.vRenderOrder, k)
+					table.insert(self.vRenderOrder, key)
 				end
 			end
 		end
 
-		for k, name in ipairs(self.vRenderOrder) do
+		for _, name in ipairs(self.vRenderOrder) do
 			local v = self.VElements[name]
 			if not v then
 				self.vRenderOrder = nil
@@ -296,45 +278,57 @@ if CLIENT then
 			end
 
 			if v.hide then continue end
-			local model = v.modelEnt
-			local sprite = v.spriteMaterial
-			if not v.bone then continue end
-			local pos, ang = self:GetBoneOrientation(self.VElements, v, vm)
-			if not pos then continue end
-			if v.type == "Model" and IsValid(model) then
-				model:SetPos(pos + ang:Forward() * v.pos.x + ang:Right() * v.pos.y + ang:Up() * v.pos.z)
+			if v.type == "Model" and IsValid(v.modelEnt) then
+				local pos, ang = self:GetBoneOrientation(self.VElements, v, vm)
+				if not pos then
+					self.vRenderOrder = nil
+					break
+				end
+
+				v.modelEnt:SetPos(pos + ang:Forward() * v.pos.x + ang:Right() * v.pos.y + ang:Up() * v.pos.z)
 				ang:RotateAroundAxis(ang:Up(), v.angle.y)
 				ang:RotateAroundAxis(ang:Right(), v.angle.p)
 				ang:RotateAroundAxis(ang:Forward(), v.angle.r)
-				model:SetAngles(ang)
 				local matrix = Matrix()
 				matrix:Scale(v.size)
-				model:EnableMatrix("RenderMultiply", matrix)
+				v.modelEnt:EnableMatrix("RenderMultiply", matrix)
 				if v.material == "" then
-					model:SetMaterial("")
-				elseif model:GetMaterial() ~= v.material then
-					model:SetMaterial(v.material)
+					v.modelEnt:SetMaterial("")
+				elseif v.modelEnt:GetMaterial() ~= v.material then
+					v.modelEnt:SetMaterial(v.material)
 				end
 
-				if v.skin and v.skin ~= model:GetSkin() then model:SetSkin(v.skin) end
+				if v.skin and v.modelEnt:GetSkin() ~= v.skin then v.modelEnt:SetSkin(v.skin) end
 				if v.bodygroup then
-					for k, v in pairs(v.bodygroup) do
-						if model:GetBodygroup(k) ~= v then model:SetBodygroup(k, v) end
+					for _, bg in pairs(v.bodygroup) do
+						if v.modelEnt:GetBodygroup(_) ~= bg then v.modelEnt:SetBodygroup(_, bg) end
 					end
 				end
 
 				if v.surpresslightning then render.SuppressEngineLighting(true) end
 				render.SetColorModulation(v.color.r / 255, v.color.g / 255, v.color.b / 255)
 				render.SetBlend(v.color.a / 255)
-				model:DrawModel()
+				v.modelEnt:DrawModel()
 				render.SetBlend(1)
 				render.SetColorModulation(1, 1, 1)
 				if v.surpresslightning then render.SuppressEngineLighting(false) end
-			elseif v.type == "Sprite" and sprite then
+			elseif v.type == "Sprite" and v.spriteMaterial then
+				local pos, ang = self:GetBoneOrientation(self.VElements, v, vm)
+				if not pos then
+					self.vRenderOrder = nil
+					break
+				end
+
 				local drawpos = pos + ang:Forward() * v.pos.x + ang:Right() * v.pos.y + ang:Up() * v.pos.z
-				render.SetMaterial(sprite)
+				render.SetMaterial(v.spriteMaterial)
 				render.DrawSprite(drawpos, v.size.x, v.size.y, v.color)
 			elseif v.type == "Quad" and v.draw_func then
+				local pos, ang = self:GetBoneOrientation(self.VElements, v, vm)
+				if not pos then
+					self.vRenderOrder = nil
+					break
+				end
+
 				local drawpos = pos + ang:Forward() * v.pos.x + ang:Right() * v.pos.y + ang:Up() * v.pos.z
 				ang:RotateAroundAxis(ang:Up(), v.angle.y)
 				ang:RotateAroundAxis(ang:Right(), v.angle.p)
@@ -347,30 +341,29 @@ if CLIENT then
 	end
 
 	function SWEP:GetBoneOrientation(basetab, tab, ent, bone_override)
-		local bone, pos, ang
 		if tab.rel and tab.rel ~= "" then
-			local v = basetab[tab.rel]
-			if not v then return end
-			pos, ang = self:GetBoneOrientation(basetab, v, ent)
+			local rel = basetab[tab.rel]
+			local pos, ang = self:GetBoneOrientation(basetab, rel, ent)
 			if not pos then return end
-			pos = pos + ang:Forward() * v.pos.x + ang:Right() * v.pos.y + ang:Up() * v.pos.z
-			ang:RotateAroundAxis(ang:Up(), v.angle.y)
-			ang:RotateAroundAxis(ang:Right(), v.angle.p)
-			ang:RotateAroundAxis(ang:Forward(), v.angle.r)
-		else
-			bone = ent:LookupBone(bone_override or tab.bone)
-			if not bone then return end
-			pos, ang = Vector(0, 0, 0), Angle(0, 0, 0)
-			local m = ent:GetBoneMatrix(bone)
-			if m then pos, ang = m:GetTranslation(), m:GetAngles() end
-			if IsValid(self:GetOwner()) and self:GetOwner():IsPlayer() and ent == self:GetOwner():GetViewModel() and self.ViewModelFlip then ang.r = -ang.r end
+			pos = pos + ang:Forward() * rel.pos.x + ang:Right() * rel.pos.y + ang:Up() * rel.pos.z
+			ang:RotateAroundAxis(ang:Up(), rel.angle.y)
+			ang:RotateAroundAxis(ang:Right(), rel.angle.p)
+			ang:RotateAroundAxis(ang:Forward(), rel.angle.r)
+			return pos, ang
 		end
+
+		local bone = ent:LookupBone(bone_override or tab.bone)
+		if not bone then return end
+		local m = ent:GetBoneMatrix(bone)
+		if not m then return end
+		local pos, ang = m:GetTranslation(), m:GetAngles()
+		if IsValid(self:GetOwner()) and self:GetOwner():IsPlayer() and ent == self:GetOwner():GetViewModel() and self.ViewModelFlip then ang.r = -ang.r end
 		return pos, ang
 	end
 
 	function SWEP:CreateModels(tab)
 		if not tab then return end
-		for k, v in pairs(tab) do
+		for _, v in pairs(tab) do
 			if v.type == "Model" and v.model and v.model ~= "" and (not IsValid(v.modelEnt) or v.createdModel ~= v.model) and string.find(v.model, ".mdl") and file.Exists(v.model, "GAME") then
 				v.modelEnt = ClientsideModel(v.model, RENDER_GROUP_VIEW_MODEL_OPAQUE)
 				if IsValid(v.modelEnt) then
@@ -389,9 +382,9 @@ if CLIENT then
 				}
 
 				local tocheck = {"nocull", "additive", "vertexalpha", "vertexcolor", "ignorez"}
-				for i, j in pairs(tocheck) do
-					if v[j] then
-						params["$" .. j] = 1
+				for _, flag in ipairs(tocheck) do
+					if v[flag] then
+						params["$" .. flag] = 1
 						name = name .. "1"
 					else
 						name = name .. "0"
@@ -407,49 +400,39 @@ if CLIENT then
 	local allbones
 	local hasGarryFixedBoneScalingYet = false
 	function SWEP:UpdateBonePositions(vm)
-		if self.ViewModelBoneMods then
-			if not vm:GetBoneCount() then return end
-			local loopthrough = self.ViewModelBoneMods
+		if not vm:GetBoneCount() then return end
+		local loopthrough = self.ViewModelBoneMods
+		if not hasGarryFixedBoneScalingYet then
+			allbones = {}
+			for i = 0, vm:GetBoneCount() do
+				local bonename = vm:GetBoneName(i)
+				allbones[bonename] = self.ViewModelBoneMods[bonename] or {
+					scale = Vector(1, 1, 1),
+					pos = Vector(0, 0, 0),
+					angle = Angle(0, 0, 0)
+				}
+			end
+
+			loopthrough = allbones
+		end
+
+		for k, v in pairs(loopthrough) do
+			local bone = vm:LookupBone(k)
+			if not bone then continue end
+			local scale, pos, ang = v.scale, v.pos, v.angle
 			if not hasGarryFixedBoneScalingYet then
-				allbones = {}
-				for i = 0, vm:GetBoneCount() do
-					local bonename = vm:GetBoneName(i)
-					if self.ViewModelBoneMods[bonename] then
-						allbones[bonename] = self.ViewModelBoneMods[bonename]
-					else
-						allbones[bonename] = {
-							scale = Vector(1, 1, 1),
-							pos = Vector(0, 0, 0),
-							angle = Angle(0, 0, 0)
-						}
-					end
+				local ms, cur = Vector(1, 1, 1), vm:GetBoneParent(bone)
+				while cur >= 0 do
+					ms = ms * (loopthrough[vm:GetBoneName(cur)].scale or Vector(1, 1, 1))
+					cur = vm:GetBoneParent(cur)
 				end
 
-				loopthrough = allbones
+				scale = scale * ms
 			end
 
-			for k, v in pairs(loopthrough) do
-				local bone = vm:LookupBone(k)
-				if not bone then continue end
-				local s = Vector(v.scale.x, v.scale.y, v.scale.z)
-				local p = Vector(v.pos.x, v.pos.y, v.pos.z)
-				local ms = Vector(1, 1, 1)
-				if not hasGarryFixedBoneScalingYet then
-					local cur = vm:GetBoneParent(bone)
-					while cur >= 0 do
-						local pscale = loopthrough[vm:GetBoneName(cur)].scale
-						ms = ms * pscale
-						cur = vm:GetBoneParent(cur)
-					end
-				end
-
-				s = s * ms
-				if vm:GetManipulateBoneScale(bone) ~= s then vm:ManipulateBoneScale(bone, s) end
-				if vm:GetManipulateBoneAngles(bone) ~= v.angle then vm:ManipulateBoneAngles(bone, v.angle) end
-				if vm:GetManipulateBonePosition(bone) ~= p then vm:ManipulateBonePosition(bone, p) end
-			end
-		else
-			self:ResetBonePositions(vm)
+			if vm:GetManipulateBoneScale(bone) ~= scale then vm:ManipulateBoneScale(bone, scale) end
+			if vm:GetManipulateBoneAngles(bone) ~= ang then vm:ManipulateBoneAngles(bone, ang) end
+			if vm:GetManipulateBonePosition(bone) ~= pos then vm:ManipulateBonePosition(bone, pos) end
 		end
 	end
 
@@ -460,22 +443,5 @@ if CLIENT then
 			vm:ManipulateBoneAngles(i, Angle(0, 0, 0))
 			vm:ManipulateBonePosition(i, Vector(0, 0, 0))
 		end
-	end
-
-	function table.FullCopy(tab)
-		if not tab then return nil end
-		local res = {}
-		for k, v in pairs(tab) do
-			if type(v) == "table" then
-				res[k] = table.FullCopy(v)
-			elseif type(v) == "Vector" then
-				res[k] = Vector(v.x, v.y, v.z)
-			elseif type(v) == "Angle" then
-				res[k] = Angle(v.p, v.y, v.r)
-			else
-				res[k] = v
-			end
-		end
-		return res
 	end
 end
