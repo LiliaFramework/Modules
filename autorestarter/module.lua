e1e0bfc4ba667919c45216b91a08bb4a1e971d35
Samael <33399712+bleonheart@users.noreply.github.com>
2025-07-10 11:54:15 +0100

    UpperCasing

--- autorestarter/module.lua
+++ autorestarter/module.lua
@@ -3,5 +3,5 @@ MODULE.author = "Samael"
 MODULE.discord = "@liliaplayer"
 MODULE.version = "1.0.3"
 MODULE.desc = "Automatically restarts the server every interval."
-MODULE.public = true
+MODULE.Public = true
 MODULE.Features = {}
--- cards/module.lua
+++ cards/module.lua
@@ -3,5 +3,5 @@ MODULE.author = "Samael"
 MODULE.discord = "@liliaplayer"
 MODULE.version = "1.0.2"
 MODULE.desc = "Adds the ability to draw a random card from a card deck"
-MODULE.public = true
+MODULE.Public = true
 MODULE.Features = {}
