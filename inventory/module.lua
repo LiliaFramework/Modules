7a2b1e47f253908c9f8c593ba5ee8c7ba9dba072
Samael <33399712+bleonheart@users.noreply.github.com>
2025-07-12 07:23:05 +0100

    Update module.lua

--- inventory/module.lua
+++ inventory/module.lua
@@ -5,8 +5,8 @@ MODULE.version = "1.0.3"
 MODULE.desc = "Adds a weight inventory type."
 MODULE.Public = true
 MODULE.enabled = function()
-    local path = MODULE.folder
-    if not path:find("/preload/") then return false, "Module must be located in a 'preload' subfolder (current path: '" .. path .. "')" end
+    local path = debug.getinfo(1, "S").source:sub(2)
+    if not path:find("/preload/") then return false, ("Module must be in a “preload” subfolder (current path: '%s')"):format(path) end
     return true
 end
 
@@ -14,7 +14,7 @@ MODULE.Dependencies = {
     {
         File = "weightinv.lua",
         Realm = "shared"
-    },
+    }
 }
 
-MODULE.Features = {"Adds an inventory limited by item weight", "Adds a shared weightinv library file", "Adds a new inventory type",}
+MODULE.Features = {"Adds an inventory limited by item weight", "Adds a shared weightinv library file", "Adds a new inventory type"}
