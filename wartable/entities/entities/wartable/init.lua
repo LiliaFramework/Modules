3420dbb0b09eedd558843268efe230f9fad37b15
Samael <33399712+bleonheart@users.noreply.github.com>
2025-07-14 10:57:49 +0100

    Add pre/post hooks for wartable, whitelist, and wordfilter

--- wartable/entities/entities/wartable/init.lua
+++ wartable/entities/entities/wartable/init.lua
@@ -2,9 +2,11 @@
 AddCSLuaFile("cl_init.lua")
 AddCSLuaFile("shared.lua")
 function ENT:Use(activator)
+    hook.Run("PreWarTableUsed", activator, self, activator:KeyDown(IN_SPEED))
     net.Start("UseWarTable")
     net.WriteEntity(self)
     net.WriteBool(activator:KeyDown(IN_SPEED))
     net.Send(activator)
     hook.Run("WarTableUsed", activator, self, activator:KeyDown(IN_SPEED))
+    hook.Run("PostWarTableUsed", activator, self, activator:KeyDown(IN_SPEED))
 end
