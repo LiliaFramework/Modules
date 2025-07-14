d50457b0a1091f0315a78ce2df8fddf0a2a383b4
Samael <33399712+bleonheart@users.noreply.github.com>
2025-07-14 10:06:42 +0100

    Update module.lua

--- slowweapons/module.lua
+++ slowweapons/module.lua
@@ -3,9 +3,8 @@ MODULE.author = "Samael"
 MODULE.discord = "@liliaplayer"
 MODULE.version = 1.05
 MODULE.desc = "Slows player movement when wielding specified heavy weapons to encourage tactical choices."
+MODULE.Public = true
+MODULE.Features = {"Adds slower movement while holding heavy weapons", "Adds speed penalties defined per weapon", "Adds encouragement for strategic choices", "Adds customizable weapon speed table", "Adds automatic speed restore when switching"}
 MODULE.WeaponsSpeed = {
     ["fo3_fatman"] = 130,
 }
-
-MODULE.Public = true
-MODULE.Features = {"Adds slower movement while holding heavy weapons", "Adds speed penalties defined per weapon", "Adds encouragement for strategic choices", "Adds customizable weapon speed table", "Adds automatic speed restore when switching"}
