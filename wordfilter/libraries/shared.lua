aded56ada24e0e997dd9889837533331ae20a71e
Samael <33399712+bleonheart@users.noreply.github.com>
2025-07-16 04:03:14 +0100

    Update shared.lua

--- wordfilter/libraries/shared.lua
+++ wordfilter/libraries/shared.lua
@@ -1,64 +1,41 @@
-local blacklist = {
-    "nigger",
-    "faggot",
-    "chink",
-    "kike",
-    "spic",
-    "gook",
-    "wetback",
-    "dyke",
-    "tranny",
-    "retard",
-    "coon",
-    "raghead",
-    "nip",
-    "honky",
-    "gyp",
-    "beaner",
-    "paki",
-    "slant",
-    "slope",
-    "jap"
-}
-
+MODULE.WordBlackList = {"nigger", "faggot", "chink", "kike", "spic", "gook", "wetback", "dyke", "tranny", "retard", "coon", "raghead", "nip", "honky", "gyp", "beaner", "paki", "slant", "slope", "jap"}
 function MODULE:LoadData()
     local stored = self:getData({})
     if istable(stored) then
         for _, word in ipairs(stored) do
-            if not table.HasValue(blacklist, word) then
-                table.insert(blacklist, word)
-            end
+            if not table.HasValue(self.WordBlackList, word) then table.insert(self.WordBlackList, word) end
         end
     end
 end
 
 function MODULE:SaveData()
-    self:setData(blacklist)
+    self:setData(self.WordBlackList)
 end
 
 function MODULE:AddBlacklistedWord(word)
     if not word then return end
-    if table.HasValue(blacklist, word) then return end
-    table.insert(blacklist, word)
+    if table.HasValue(self.WordBlackList, word) then return end
+    table.insert(self.WordBlackList, word)
     self:SaveData()
     hook.Run("WordAddedToFilter", word)
 end
 
 function MODULE:RemoveBlacklistedWord(word)
     if not word then return end
-    for i, v in ipairs(blacklist) do
+    for i, v in ipairs(self.WordBlackList) do
         if v == word then
-            table.remove(blacklist, i)
+            table.remove(self.WordBlackList, i)
             self:SaveData()
             hook.Run("WordRemovedFromFilter", word)
             break
         end
     end
 end
+
 function MODULE:PlayerSay(ply, text)
     hook.Run("PreFilterCheck", ply, text)
     local lowerText = text:lower()
-    for _, bad in pairs(blacklist) do
+    for _, bad in pairs(self.WordBlackList) do
         if lowerText:find(bad, 1, true) then
             hook.Run("FilteredWordUsed", ply, bad, text)
             hook.Run("PostFilterCheck", ply, text, false)
