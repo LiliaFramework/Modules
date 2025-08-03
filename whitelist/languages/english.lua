0c1b36a61d458e025fd086b073cb5fc926627529
Samael <33399712+bleonheart@users.noreply.github.com>
2025-08-04 00:10:21 +0100

    XD

--- whitelist/languages/english.lua
+++
@@ -1,5 +0,0 @@
-﻿NAME = "English"
-LANGUAGE = {
-    blacklistKick = "You are blacklisted from this server.",
-    whitelistKick = "You are not whitelisted on %s."
-}
--- whitelist/module.lua
+++
@@ -1,7 +0,0 @@
-﻿MODULE.name = "Whitelist"
-MODULE.author = "Samael"
-MODULE.discord = "@liliaplayer"
-MODULE.version = 1.16
-MODULE.desc = "Controls server access through a whitelist. Players can submit signups and admins manage entries with dedicated commands."
-MODULE.Public = true
-MODULE.Features = {"Adds a server access whitelist", "Adds toggleable player signups", "Adds a public module for gating entry", "Adds admin commands to manage the list", "Adds notifications on join attempt"}
