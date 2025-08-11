a9baeae9890ab818885115b41233271a94829aca
Samael <33399712+bleonheart@users.noreply.github.com>
2025-08-11 11:25:07 +0100

    not needed

--- inventory/items/base/bags.lua
+++
@@ -1,5 +0,0 @@
-﻿ITEM.name = "Bag"
-ITEM.desc = "A bag to hold more items."
-ITEM.model = "models/props_c17/suitcase001a.mdl"
-ITEM.category = "Storage"
-ITEM.weight = -5
--- inventory/submodules/vendor/languages/english.lua
+++
@@ -1,7 +0,0 @@
-NAME = "English"
-LANGUAGE = {
-    name = "Name",
-    saveVendorsDesc = "Save vendor data.",
-    vendorCanNotAfford = "You cannot afford this.",
-    vendorSaved = "Vendor saved."
-}
