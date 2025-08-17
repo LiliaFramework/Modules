020ee7b4c3df14f099e75b7ff631616cbbda76ee
Samael <33399712+bleonheart@users.noreply.github.com>
2025-08-17 21:22:49 +0100

    Update italian.lua

--- utilities/languages/italian.lua
+++ utilities/languages/italian.lua
@@ -1,7 +1,7 @@
 ﻿NAME = "Italian"
 LANGUAGE = {
     invalidDates = "Invalid dates",
-    invalidDate = "Invalid date provided.",
+
     invalidTimeFormat = "Invalid time format. Expected 'HH:MM:SS - DD/MM/YYYY'.",
     invalidTimeValues = "Invalid time values.",
     timeIsPast = "The specified time is in the past.",
