41f7ee41c39d8558f65a7412e7a7abce8431d09f
Samael <33399712+bleonheart@users.noreply.github.com>
2025-09-16 22:30:59 +0100

    xd

--- stungun/libraries/server.lua
+++
@@ -1,7 +0,0 @@
-﻿local function DeleteHelpEnt(ply)
-    if IsValid(ply) and IsValid(ply.HelpEnt) then ply.HelpEnt:Remove() end
-end
-
-hook.Add("PlayerDeath", "diesbitch", DeleteHelpEnt)
-hook.Add("PlayerSpawn", "spawnedbitch", DeleteHelpEnt)
-hook.Add("PlayerDisconnected", "leavingbitch", DeleteHelpEnt)
--- stungun/module.lua
+++
@@ -1,8 +0,0 @@
-﻿MODULE.name = "Stun Gun"
-MODULE.versionID = "public_stungun"
-MODULE.author = "Samael"
-MODULE.discord = "@liliaplayer"
-MODULE.version = 1.0
-MODULE.desc = "Adds a taser weapon that immobilizes targets, StunTime and MaxDist as configurable settings, optional damage and post-process effects, networked stun animations, and logs when players are tased."
-MODULE.WorkshopContent = "3432649835"
-MODULE.NetworkStrings = {"fucking_stun", "fucking_stun2", "omglaser"}
