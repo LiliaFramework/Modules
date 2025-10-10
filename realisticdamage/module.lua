11847e63a0c81423e101458c60db10dc11d0391e
Samael <33399712+bleonheart@users.noreply.github.com>
2025-10-10 04:55:36 +0100

    xd

--- realisticdamage/module.lua
+++
@@ -1,6 +0,0 @@
-﻿MODULE.name = "Realistic Damage"
-MODULE.versionID = "public_realisticdamage"
-MODULE.author = "Samael"
-MODULE.discord = "@liliaplayer"
-MODULE.version = 1.0
-MODULE.desc = "Adds damage scaling by body part, custom multipliers for realism, and adjustments to player damage output."
