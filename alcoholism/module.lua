0f0babedc1e13ddd61b38bdca0517b33a7d226db
Samael <33399712+bleonheart@users.noreply.github.com>
2025-10-13 05:59:31 +0100

    xd

--- alcoholism/module.lua
+++ alcoholism/module.lua
@@ -2,5 +2,5 @@
 MODULE.versionID = "public_alcoholism"
 MODULE.author = "Samael"
 MODULE.discord = "@liliaplayer"
-MODULE.version = 1.0
+MODULE.version = 1.2
 MODULE.desc = "Adds drinkable alcohol that increases a player's intoxication level. High BAC blurs vision and slows movement until the effect wears off."
