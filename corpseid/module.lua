2960bbfdc62ea0f12ceafa5f0f927975214dbea9
Samael <33399712+bleonheart@users.noreply.github.com>
2025-11-03 23:46:47 +0000

    XD

--- corpseid/module.lua
+++
@@ -1,6 +0,0 @@
-﻿MODULE.name = "Corpse ID"
-MODULE.versionID = "public_corpseid"
-MODULE.author = "Samael"
-MODULE.discord = "@liliaplayer"
-MODULE.version = 1.2
-MODULE.desc = "Adds the ability to identify corpses after IdentificationTime, use of CorpseMessageFont for 3D2D text, info display above bodies, configurable identification delays, and logs when corpses are identified."
