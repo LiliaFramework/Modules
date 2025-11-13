9cad229d44bca4f3d73d86f9967986fb0235a03a
Samael <33399712+bleonheart@users.noreply.github.com>
2025-11-13 07:19:09 +0000

    XD

--- viewbob/module.lua
+++
@@ -1,6 +0,0 @@
-MODULE.name = "View Bob"
-MODULE.versionID = "public_viewbob"
-MODULE.author = "Samael"
-MODULE.discord = "@liliaplayer"
-MODULE.version = 1.2
-MODULE.desc = "Adds camera bobbing while moving, adjustable intensity, hooks to modify view punch, and configuration for bobbing frequency."
