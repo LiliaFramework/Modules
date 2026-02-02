100f9deac7e8e765bd04c8287fac9ba2c3806d9f
Samael <33399712+bleonheart@users.noreply.github.com>
2026-02-02 01:49:23 +0000

    Update module.lua

--- slots/module.lua
+++ slots/module.lua
@@ -7,4 +7,9 @@ MODULE.desc = "Adds a slot machine minigame, a workshop model for the machine, h
 MODULE.WorkshopContent = "3527541056"
 MODULE.Changelog = {
     ["1.0"] = "Initial Release",
+    ["2.0"] = "SAF",
+    ["3.0"] = "SADAFD",
+    ["4.0"] = "InitiaASFASFAl Release",
+    ["5.0"] = "InitialAFAFSAF Release",
+    ["6.0"] = "InitiFASFSAFAFSAal Release",
 }
