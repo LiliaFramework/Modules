25c560c5722cc4d33eb4c331de08a5c599d22f8f
Samael <33399712+bleonheart@users.noreply.github.com>
2026-04-24 21:05:05 +0100

    Removed Bodygrouper

--- bodygrouper/config.lua
+++
@@ -1,5 +0,0 @@
-﻿lia.config.add("BodyGrouperModel", "bodyGrouperModel", "models/props_c17/FurnitureDresser001a.mdl", nil, {
-    desc = "bodyGrouperModelDesc",
-    category = "gameplay",
-    type = "Generic"
-})
